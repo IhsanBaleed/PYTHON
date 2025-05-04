02f10bca12809a4f1f93b00c0ca4d70a242fb371
ibaleed <ibaleed@magnite.com>
2025-05-05 00:21:33 +0100

    Added Query Param Handle

--- API/api.py
+++ API/api.py
@@ -1,7 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from functions import *
 
+# this is a rest API server
 app = Flask(__name__)
+
+data = [
+    {"id": 1, "name": "Item One"},
+    {"id": 2, "name": "Item Two"},
+    {"id": 3, "name": "Item Three"},
+    {"id": 4, "name": "Item Four"},
+]
 
 @app.route('/')
 def home():
@@ -19,9 +27,19 @@ def Func2_call():
 def Func3_call():
     return Func3()
 
-@app.route('/items/<int:item_id>')
-def get_item(item_id):
-    return jsonify({"item_id": item_id})
+@app.route('/items', methods=['GET'])
+def get_items():
+    name_filter = request.args.get('name')  # optional query param ?name=Item
+    limit = request.args.get('limit', type=int)  # optional query param ?limit=2
+
+    filtered = data
+    if name_filter:
+        filtered = [item for item in filtered if name_filter.lower() in item['name'].lower()]
+    
+    if limit:
+        filtered = filtered[:limit]
+
+    return jsonify(filtered)
 
 if __name__ == '__main__':
     app.run(debug=True)
